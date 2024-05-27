aac662fa33dc0f206f042606b4544698572c1792
Jack <jack@hopsoffice.com>
2024-05-28 08:41:18 +0900

    add 1.1.7 example

---
+++ src/chapter1/1.1.7.clj
@@ -0,0 +1,23 @@
+(ns chapter1.1.1.7)
+
+(defn average [x y]
+  (/ (+ x y) 2))
+
+(defn improve [guess x]
+  (average guess (/ x guess)))
+
+(defn square [x] (* x x))
+
+(defn good-enough? [guess x]
+  (< (Math/abs (- (square guess) x)) 0.001))
+
+(defn sqrt-iter [guess x]
+  (if (good-enough? guess x)
+    guess
+    (sqrt-iter (improve guess x) x)))
+
+(defn sqrt [x]
+  (sqrt-iter 1 x))
+
+(sqrt 9)
+
