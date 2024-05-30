878f9a715531629fc24303bc4db6ac9dff582300
Jack <jack@hopsoffice.com>
2024-05-30 20:31:59 +0900

    finish 1.2.1

---
+++ src/chapter1/1.2.1.clj
@@ -0,0 +1,20 @@
+(ns chapter1.1.2.1)
+
+(defn factorial1 [n]
+  (if (= n 1)
+      1
+      (* n (factorial1 (- n 1)))))
+
+(factorial1 5)
+
+(defn fact-iter [product counter max-count]
+    (if (> counter max-count)
+        product
+        (fact-iter (* counter product)
+                   (+ counter 1)
+                   max-count)))
+
+(defn factorial2 [n]
+  (fact-iter 1 1 n))
+
+(factorial2 5)
